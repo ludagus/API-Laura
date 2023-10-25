9b34bda52ed1a2a67a3ac6c4f75fc734e511673c
ludagus <profelaurapared@gmail.com>
2023-10-25 17:14:32 -0300

    Versión con Manejo de exepciones

--- src/controller.js
+++ src/controller.js
@@ -21,21 +21,49 @@ class LibroController{
 //Añadir un nuevo registro
     async add(req, res){
         const libro= req.body;
-        const [result]= await pool.query(`INSERT INTO libros(nombre, autor, categoria, año, ISBN) VALUES (?, ?, ?,?,?)`, [libro.nombre, libro.autor, libro.categoria, libro.año, libro.ISBN]);
-        res.json({"Id insertado": result.insertId});
+        const listaAtributos= ['nombre', 'autor','categoria', 'año', 'ISBN'];
+        const atributosExtra= Object.keys(libro).filter(attr=> !listaAtributos.includes(attr));
+
+        if(atributosExtra.length>0){
+            return res.json({error:`Atributos invalidos: ${atributosExtra.join(',')}`});
+        }
+        try{  
+            const [result]= await pool.query(`INSERT INTO libros(nombre, autor, categoria, año, ISBN) VALUES (?, ?, ?,?,?)`, [libro.nombre, libro.autor, libro.categoria, libro.año, libro.ISBN]);
+            res.json({"Id insertado": result.insertId});
+        }catch(error){
+            console.log('Error al añadir el registro', error);
+        }   
     }
   
 //Modificar un registro
     async update(req, res){
-        const libro= req.body;
-        const [result]= await pool.query(`UPDATE libros SET nombre=(?), autor=(?), categoria=(?), año=(?), ISBN=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.año, libro.ISBN, libro.id]);
-        res.json({"Registros actualizados": result.changeRows});
+        try {
+            const libro= req.body;
+            const [result]= await pool.query(`UPDATE libros SET nombre=(?), autor=(?), categoria=(?), año=(?), ISBN=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.año, libro.ISBN, libro.id]);
+            
+            if (result.affectedRows >0) {
+                res.status(404).send("No se encontró el registro");
+            } else {
+                res.json({"Registros actualizados": result.changeRows});
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
+
 //Eliminar un registro
     async delete(req, res){
-        const libro= req.body;
-        const [result]= await pool.query(`DELETE FROM libros WHERE ISBN=(?)`,  [libro.ISBN]);
-        res.json({"Registros eliminado": result.affectedRows});
-    }
+        try {
+            const libro= req.body;
+            const [result]= await pool.query(`DELETE FROM libros WHERE ISBN=(?)`,  [libro.ISBN]);
+            if (result.affectedRows === 0) {
+                res.status(404).send("No se encontró el registro");
+            } else {
+                res.json({"Registros eliminado": result.affectedRows});
+            }
+        } catch (error) {
+            console.error(error);
+        }
+}
 }
 export const libro= new LibroController();
